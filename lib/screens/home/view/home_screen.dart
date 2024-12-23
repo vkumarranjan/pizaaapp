830d249db1ef8334a57c74842e3f98252bca9a78
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-12-23 15:18:29 +0530

    updated the text

--- lib/screens/home/view/home_screen.dart
+++ lib/screens/home/view/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('vinay'),
+        child: Text('Home screen'),
       ),
     );
   }
